3fef2b68e9fddfeb405e09b02adfcefb53e4d710
zhoujt1994 <jiz509@eng.ucsd.edu>
2019-04-14 09:20:42 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+schicluster_version = '1.0'
+
+setup(
+    name='schicluster',
+    version=schicluster_version,
+    author='Jingtian Zhou',
+    author_email='jiz509@eng.ucsd.edu',
+    packages=find_packages(),
+    description='A package for single-cell Hi-C data clustering and downstream analysis.',
+    long_description=open('README.md').read(),
+    include_package_data=True,
+    install_requires=['numpy', 'scipy', 'scikit-learn']
+)
