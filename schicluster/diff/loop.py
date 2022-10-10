ca5333dfee5654d508de97cebed1b043015efbb7
hq <liuhq@ucsd.edu>
2022-10-09 23:25:35 -0700

    Update loop.py

--- schicluster/diff/loop.py
+++ schicluster/diff/loop.py
@@ -3,7 +3,7 @@ import pandas as pd
 from scipy.stats import f as f_dist
 
 
-def one_way_anova(chrom_loop_ds, da_name, value_type, group_n_name='group_n', group_dim='sample_id'):
+def one_way_anova(chrom_loop_ds, da_name, value_type, group_n_dim='group_n', group_dim='sample_id'):
     """
     Perform one-way ANOVA on a single-chrom loop dataset.
 
@@ -16,7 +16,7 @@ def one_way_anova(chrom_loop_ds, da_name, value_type, group_n_name='group_n', gr
     value_type
         The value type of the data array to perform ANOVA on.
         both "{value_type}" and "{value_type}2" should be present in the "{da_name}_value_type" dimension.
-    group_n_name
+    group_n_dim
         The name of the group number variable.
     group_dim
         The name of the group dimension.
@@ -27,7 +27,7 @@ def one_way_anova(chrom_loop_ds, da_name, value_type, group_n_name='group_n', gr
     """
     loop_ds = chrom_loop_ds
     # number of cells per group
-    n = loop_ds[group_n_name]
+    n = loop_ds[group_n_dim]
     n_group = n.size
     total_n = int(n.sum())
 
@@ -54,3 +54,42 @@ def one_way_anova(chrom_loop_ds, da_name, value_type, group_n_name='group_n', gr
     p = f_dist(n_group - 1, total_n - n_group).sf(f)
     p = pd.Series(p, index=f.index)
     return f, p
+
+
+def merge_groups(loop_ds, group_map, da_name, group_dim='sample_id', group_n_dim='group_n'):
+    """
+    Merge groups into larger groups in a loop dataset.
+
+    Parameters
+    ----------
+    loop_ds
+        A loop dataset.
+    group_map
+        A pd.Series mapping from old group names to new group names.
+    da_name
+        The name of the data array to merge groups for.
+    group_dim
+        The name of the group dimension.
+    group_n_dim
+        The name of the group number variable.
+
+    Returns
+    -------
+    A loop dataset with merged groups.
+    """
+    loop_ds[da_name] = loop_ds[da_name] * loop_ds.coords[group_n_dim]
+
+    loop_ds['_sample_group'] = group_map
+    loop_ds = loop_ds.groupby('_sample_group').sum(dim=group_dim)
+
+    cell_count = loop_ds.coords[group_n_dim].to_pandas()
+    sample_group_count = cell_count.groupby(group_map).sum()
+    sample_group_count.index.name = '_sample_group'
+    loop_ds.coords[group_n_dim] = sample_group_count
+
+    loop_ds[da_name] = loop_ds[da_name] / loop_ds[group_n_dim]
+
+    loop_ds = loop_ds.rename({
+        '_sample_group': group_dim
+    })
+    return loop_ds
